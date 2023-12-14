db728f3ae994102b92559f464de5f963fb463798
PavelStepantsov <pvl.vnz@mail.ru>
2023-12-14 07:51:23 +0300

    перводе дз.спринт 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
-
+import java.util.Scanner;
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("На скольких человек необходимо разделить счет");
+        int persons = 0;
+        while (true) {
+            persons = scanner.nextInt();
+            if (persons == 1) {
+                System.out.println("Вы один и вам придется полностью оплатить счет");
+            } else if (persons < 0) {
+                System.out.println("Ошибка, введите корректное значение");
+            }
+            if (persons > 1) {
+                break;
+            }
+        }
+        Calculator calc = new Calculator();
+    calc.calculate();
+    calc.vivod();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
